5b1801939d2888a2b2c863a0e4ede072d9315064
Timilehin <aregbestimi@gmail.com>
2020-08-31 15:43:48 +0100

    added koin

---
+++ app/src/main/java/com/android/example/mathalarm/app/AlarmApplication.kt
@@ -0,0 +1,20 @@
+package com.android.example.mathalarm.app
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import timber.log.Timber
+
+class AlarmApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@AlarmApplication)
+            modules(listOf())
+        }
+    }
+}
