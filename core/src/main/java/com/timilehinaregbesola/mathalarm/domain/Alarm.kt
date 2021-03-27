30170178013c31350f02d7569d914b6f6d66c2a0
Timilehin <aregbestimi@gmail.com>
2021-03-27 08:32:15 +0100

    created core module and added Alarm domain

---
+++ core/src/main/java/com/timilehinaregbesola/mathalarm/domain/Alarm.kt
@@ -0,0 +1,31 @@
+package com.timilehinaregbesola.mathalarm.domain
+
+import java.util.*
+
+data class Alarm(
+    var alarmId: Long = 0L,
+
+    val newCal: Calendar = Calendar.getInstance(),
+
+    val newHour: Int = newCal[Calendar.HOUR_OF_DAY],
+
+    val newMinute: Int = newCal[Calendar.MINUTE],
+
+    var hour: Int = newHour,
+
+    var minute: Int = newMinute,
+
+    var repeat: Boolean = false,
+
+    var repeatDays: String = "FFFFFFF",
+
+    var isOn: Boolean = false,
+
+    var difficulty: Int = 0,
+
+    var alarmTone: String = "",
+
+    var vibrate: Boolean = false,
+
+    var snooze: Int = 5
+)
