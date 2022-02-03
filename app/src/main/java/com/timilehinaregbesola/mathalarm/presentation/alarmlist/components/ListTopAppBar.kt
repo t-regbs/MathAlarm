29f3e052530b68ce3e6265c325c080ef6497ed9a
Timilehin <aregbestimi@gmail.com>
2022-02-03 14:46:49 +0100

    updated overflow menu

--- app/src/main/java/com/timilehinaregbesola/mathalarm/presentation/alarmlist/components/ListTopAppBar.kt
+++ app/src/main/java/com/timilehinaregbesola/mathalarm/presentation/alarmlist/components/ListTopAppBar.kt
@@ -3,10 +3,7 @@ package com.timilehinaregbesola.mathalarm.presentation.alarmlist.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.DeleteSweep
@@ -14,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -22,6 +20,7 @@ import androidx.compose.ui.unit.sp
 fun ListTopAppBar(
     openDialog: MutableState<Boolean>
 ) {
+    val showSettings = rememberSaveable { mutableStateOf(false) }
     TopAppBar(
         title = {
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
@@ -35,8 +34,21 @@ fun ListTopAppBar(
             IconButton(onClick = { openDialog.value = true }) {
                 Icon(imageVector = Icons.Outlined.DeleteSweep, contentDescription = null)
             }
-            IconButton(onClick = { }) {
+            IconButton(onClick = { showSettings.value = !showSettings.value }) {
                 Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "More")
+            }
+            DropdownMenu(
+                expanded = showSettings.value,
+                onDismissRequest = { showSettings.value = false }
+            ) {
+                DropdownMenuItem(
+                    onClick = {
+                        // Navigate to settings
+                        showSettings.value = false
+                    }
+                ) {
+                    Text(text = "Settings")
+                }
             }
         }
     )
