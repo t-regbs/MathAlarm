e191328d3ad8dad7baef0b6405de976856aa141d
Timilehin <aregbestimi@gmail.com>
2022-02-17 15:32:32 +0100

    added schedule next alarm tests

---
+++ app/src/test/java/com/timilehinaregbesola/mathalarm/usecases/ScheduleNextAlarmTest.kt
@@ -0,0 +1,54 @@
+package com.timilehinaregbesola.mathalarm.usecases
+
+import com.timilehinaregbesola.mathalarm.data.AlarmRepository
+import com.timilehinaregbesola.mathalarm.domain.model.Alarm
+import com.timilehinaregbesola.mathalarm.fake.AlarmInteractorFake
+import com.timilehinaregbesola.mathalarm.fake.AlarmRepositoryFake
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class ScheduleNextAlarmTest {
+    private val dataSource = AlarmRepositoryFake()
+
+    private val alarmRepository = AlarmRepository(dataSource)
+
+    private val alarmInteractor = AlarmInteractorFake()
+
+    private val addAlarmUseCase = AddAlarm(alarmRepository)
+
+    private val scheduleNextAlarmUseCase = ScheduleNextAlarm(alarmInteractor)
+
+    private val baseAlarm = Alarm(alarmId = 2, title = "new alarm", isOn = true)
+
+    @Before
+    fun setup() = runBlockingTest {
+        alarmRepository.clear()
+        alarmInteractor.clear()
+    }
+
+    @Test
+    fun `test if alarm is scheduled`() = runBlockingTest {
+        val alarm = baseAlarm.copy(repeat = true)
+        addAlarmUseCase(alarm)
+
+        scheduleNextAlarmUseCase(alarm)
+
+        Assert.assertTrue(alarmInteractor.isAlarmScheduled(alarm))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `test if fails if not repeating`() = runBlockingTest {
+        val alarm = baseAlarm.copy(repeat = false)
+        scheduleNextAlarmUseCase(alarm)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `test if fails if not on`() = runBlockingTest {
+        val alarm = baseAlarm.copy(repeat = true, isOn = false)
+        scheduleNextAlarmUseCase(alarm)
+    }
+}
