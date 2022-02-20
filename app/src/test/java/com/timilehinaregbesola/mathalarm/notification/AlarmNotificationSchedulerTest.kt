32e915d7c1406f0b3c95e16ae65445316492a60b
Timilehin <aregbestimi@gmail.com>
2022-02-20 21:15:26 +0100

    fix build

--- app/src/test/java/com/timilehinaregbesola/mathalarm/notification/AlarmNotificationSchedulerTest.kt
+++ app/src/test/java/com/timilehinaregbesola/mathalarm/notification/AlarmNotificationSchedulerTest.kt
@@ -1,37 +1,33 @@
-// package com.timilehinaregbesola.mathalarm.notification
-//
-// import android.content.Context
-// import com.timilehinaregbesola.mathalarm.domain.model.Alarm
-// import com.timilehinaregbesola.mathalarm.utils.cancelAlarm
-// import com.timilehinaregbesola.mathalarm.utils.setAlarm
-// import io.mockk.every
-// import io.mockk.mockk
-// import io.mockk.mockkStatic
-// import io.mockk.verify
-// import org.junit.Before
-// import org.junit.Test
-// import java.util.*
-//
-// class AlarmNotificationSchedulerTest {
-//
-//    private val mockContext = mockk<Context>(relaxed = true)
-//
-//    private val mockAlarm = mockk<Alarm>()
-//
-//    private val scheduler = AlarmNotificationScheduler(mockContext)
-//
-//    @Before
-//    fun setUp() {
-//        val cal = Calendar.getInstance()
-//        mockkStatic("com.timilehinaregbesola.mathalarm.utils.extension-context")
-//        every { mockAlarm.repeatDays } returns "FFFTFFF"
-//        every { mockAlarm.alarmId } returns 22L
-//        every { mockAlarm.repeat } returns false
-//        every { mockAlarm.newCal } returns cal
-//        every { mockAlarm.hour } returns cal[Calendar.HOUR_OF_DAY]
-//        every { mockAlarm.minute } returns cal[Calendar.MINUTE]
-//    }
-//
+package com.timilehinaregbesola.mathalarm.notification
+
+import android.content.Context
+import com.timilehinaregbesola.mathalarm.domain.model.Alarm
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import org.junit.Before
+import java.util.*
+
+class AlarmNotificationSchedulerTest {
+
+    private val mockContext = mockk<Context>(relaxed = true)
+
+    private val mockAlarm = mockk<Alarm>()
+
+    private val scheduler = AlarmNotificationScheduler(mockContext)
+
+    @Before
+    fun setUp() {
+        val cal = Calendar.getInstance()
+        mockkStatic("com.timilehinaregbesola.mathalarm.utils.extension-context")
+        every { mockAlarm.repeatDays } returns "FFFTFFF"
+        every { mockAlarm.alarmId } returns 22L
+        every { mockAlarm.repeat } returns false
+        every { mockAlarm.newCal } returns cal
+        every { mockAlarm.hour } returns cal[Calendar.HOUR_OF_DAY]
+        every { mockAlarm.minute } returns cal[Calendar.MINUTE]
+    }
+
 //    @Test
 //    fun `check if alarm scheduled is valid`() {
 //        scheduler.scheduleAlarm(mockAlarm, false)
@@ -43,4 +39,4 @@
 //        scheduler.cancelAlarm(mockAlarm)
 //        verify { mockContext.cancelAlarm(any()) }
 //    }
-// }
+}
