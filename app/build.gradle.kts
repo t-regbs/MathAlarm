7d2a0e1580ab22990c20f9b721e82729bf4ce571
Timilehin <aregbestimi@gmail.com>
2022-02-22 21:00:56 +0100

    remove redundant block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,18 +53,6 @@ android {
     testOptions {
         unitTests.isReturnDefaultValues = true
     }
-
-    packagingOptions {
-        resources.excludes.add("META-INF/DEPENDENCIES")
-        resources.excludes.add("META-INF/LICENSE")
-        resources.excludes.add("META-INF/LICENSE.txt")
-        resources.excludes.add("META-INF/license.txt")
-        resources.excludes.add("META-INF/NOTICE")
-        resources.excludes.add("META-INF/NOTICE.txt")
-        resources.excludes.add("META-INF/notice.txt")
-        resources.excludes.add("META-INF/ASL2.0")
-        resources.excludes.add("META-INF/*.kotlin_module")
-    }
 }
 
 dependencies {
