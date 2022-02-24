d79681f0cb9dd38fde2486ca80e4d2ae6fadd8c2
Timilehin <aregbestimi@gmail.com>
2022-02-24 10:47:01 +0100

    added spacing to materialtheme

--- app/src/main/java/com/timilehinaregbesola/mathalarm/presentation/ui/Theme.kt
+++ app/src/main/java/com/timilehinaregbesola/mathalarm/presentation/ui/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
@@ -31,15 +32,6 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     onBackground = Color.Black,
     onSecondary = Color.White
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -50,10 +42,12 @@ fun MathAlarmTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composa
         LightColorPalette
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colors = colors,
+            typography = typography,
+            shapes = shapes,
+            content = content
+        )
+    }
 }
