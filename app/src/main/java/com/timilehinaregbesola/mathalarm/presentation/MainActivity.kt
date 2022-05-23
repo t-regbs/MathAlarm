44d1a556b24df0170d4e500c94320d3f759f6d5e
Timilehin <aregbestimi@gmail.com>
2022-05-23 13:00:47 +0100

    update statusbar color code

--- app/src/main/java/com/timilehinaregbesola/mathalarm/presentation/MainActivity.kt
+++ app/src/main/java/com/timilehinaregbesola/mathalarm/presentation/MainActivity.kt
@@ -1,25 +1,22 @@
 package com.timilehinaregbesola.mathalarm.presentation
 
-import android.os.Build
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowInsetsControllerCompat
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.timilehinaregbesola.mathalarm.navigation.NavGraph
 import com.timilehinaregbesola.mathalarm.presentation.appsettings.AlarmPreferencesImpl
 import com.timilehinaregbesola.mathalarm.presentation.appsettings.shouldUseDarkColors
 import com.timilehinaregbesola.mathalarm.presentation.ui.MathAlarmTheme
+import com.timilehinaregbesola.mathalarm.presentation.ui.darkPrimary
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.InternalCoroutinesApi
 import javax.inject.Inject
@@ -39,25 +36,21 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         preferences.setup()
-        window.makeTransparentStatusBar()
         setContent {
-            MathAlarmTheme(
-                darkTheme = preferences.shouldUseDarkColors()
-            ) {
-                window.statusBarColor = MaterialTheme.colors.background.toArgb()
+            val isDarkTheme = preferences.shouldUseDarkColors()
+            updateTheme(isDarkTheme)
+            MathAlarmTheme(darkTheme = isDarkTheme) {
                 NavGraph(preferences)
             }
         }
     }
-}
 
-fun Window.makeTransparentStatusBar() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-    } else {
-        setFlags(
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-        )
+    private fun updateTheme(darkTheme: Boolean) {
+        window.apply {
+            statusBarColor = if (darkTheme) darkPrimary.toArgb() else Color.WHITE
+            navigationBarColor = if (darkTheme) darkPrimary.toArgb() else Color.WHITE
+            WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars =
+                !darkTheme
+        }
     }
 }
